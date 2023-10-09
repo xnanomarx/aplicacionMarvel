d4f625cc36710e112e88a60830ea5c5f1106711b
Martin3606 <martinvaz.riv@hotmail.com>
2023-10-09 16:38:13 -0300

    a

--- intentosRaros/app/src/main/java/com/example/intentosraros/ListadoPeliculasActivity.kt
+++ intentosRaros/app/src/main/java/com/example/intentosraros/ListadoPeliculasActivity.kt
@@ -60,5 +60,68 @@ class ListadoPeliculasActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+<<<<<<< Updated upstream
+=======
+
+    private fun obtenerSeriesDesdeAPI() {
+        val client = ApiClient.apiService.fetchSeries(20) // Cambia el límite según tus necesidades
+
+        client.enqueue(object : Callback<SerieResponse> {
+            override fun onResponse(call: Call<SerieResponse>, response: Response<SerieResponse>) {
+                if (response.isSuccessful) {
+                    val seriesResponse = response.body()
+                    if (seriesResponse != null) {
+                        val series = seriesResponse.data.results
+                        actualizarRecyclerView(series)
+                    } else {
+                        Toast.makeText(this@ListadoPeliculasActivity, "Respuesta vacía", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // Maneja el caso en el que la respuesta no es exitosa
+                    Toast.makeText(this@ListadoPeliculasActivity, "Error en la respuesta", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<SerieResponse>, t: Throwable) {
+                // Maneja el caso en el que haya un error de red
+                Toast.makeText(this@ListadoPeliculasActivity, "No se pudo conectar", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+
+    private suspend fun obtenerSeriesDesdeAPICorrutina() {
+        try {
+            val series = withContext(Dispatchers.IO) {
+                val response = ApiClient.apiService.fetchSeries(20).execute()
+                if (response.isSuccessful) {
+                    response.body()?.data?.results ?: emptyList()
+                } else {
+                    emptyList()
+                }
+            }
+
+            // Actualizamos el RecyclerView en el hilo principal
+            withContext(Dispatchers.Main) {
+                actualizarRecyclerView(series)
+            }
+        } catch (e: Exception) {
+            // Manejamos errores de red u otras excepciones en el hilo principal
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@ListadoPeliculasActivity, "Error en la conexion", Toast.LENGTH_SHORT).show()
+                Log.e("API", "Error: ${e.message}", e)
+            }
+        }
+    }
+
+    private fun actualizarRecyclerView(series: List<Serie>) {
+        peliculasAdapter.peliculas.clear() // Limpia los datos existentes en el adaptador
+        peliculasAdapter.peliculas.addAll(series) // Agrega las nuevas series al adaptador
+        peliculasAdapter.notifyDataSetChanged() // Notifica al adaptador que los datos han cambiado
+    }
+
+
+
+>>>>>>> Stashed changes
 
 }
